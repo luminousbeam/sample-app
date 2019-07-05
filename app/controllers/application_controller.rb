4d4f1329f1940f22d9807a009ab7e71b838e4a18
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-05 16:49:49 -0400

    finish up user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
+# Language: Ruby, Level: Level 3
 class ApplicationController < ActionController::Base
   def hello
-  render html: "hello, world!"
+    render html: "hello, world!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# Language: Ruby, Level: Level 3
 Rails.application.routes.draw do
   resources :users
   get 'users/new'
@@ -6,4 +7,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
