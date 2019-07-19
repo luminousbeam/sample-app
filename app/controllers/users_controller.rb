a72e03adfcd2dfb88ec3f5e9c81bd2037f53c94f
luminousbeam <jocelyn.jeriah@gmail.com>
2019-07-19 18:10:55 -0400

    add functionality to edit user info, add tests accordingly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -13,18 +16,47 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-        if @user.save
-          log_in @user
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
-          # Handle a successful save.
-        else
-          render 'new'
-        end
+      if @user.save
+        log_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+        # Handle a successful save.
+      else
+        render 'new'
+      end
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = User.new(name: "Example User", email: "user@example.com")
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
     get new_user_url
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
